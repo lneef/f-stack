3ab5755dcb228c3ee830d911eecedda00d8df2cb
Lukas Neef <lukas.neef@tum.de>
2025-12-04 10:20:33 +0100

    fix wrong name

--- freebsd/sys/pcpu.h
+++ freebsd/sys/pcpu.h
@@ -206,7 +206,7 @@ struct pcpu {
 	 * reason not to keep the offsets of the MI fields constant
 	 * if only to make kernel debugging easier.
 	 */
-	PCPPCPU_MD_FIELDSU_MD_FIELDS;
+	PCPU_MD_FIELD;
 } __aligned(CACHE_LINE_SIZE);
 
 #ifdef _KERNEL
